afc40a3162781666aa78fbc6508217f4b7737257
stevenlim <stevenlimya@gmail.com>
2025-11-15 17:48:30 +0700

    d

--- movie-react/src/pages/movies/Movies.tsx
+++ movie-react/src/pages/movies/Movies.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react"
 import { NavLink } from "react-router"
 
+interface Movies{
+    _id : string,
+    judul : string,
+    tahunRilis : string,
+    sutradara : string,
+    createdAt : string,
+    updateAt : string
+}
+
 function Movies(){
-    return <div>
+    const [movies, setMovies] = useState([])
+    return <div className="container mx-auto">
+        <div className="d-flex justify-content-between mb-3">
         <h2>Movie page</h2>
-        <NavLink to="/add-movie">Add Movie</NavLink>
+        <NavLink to="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+     </div>
     </div>
 }
 export default Movies
