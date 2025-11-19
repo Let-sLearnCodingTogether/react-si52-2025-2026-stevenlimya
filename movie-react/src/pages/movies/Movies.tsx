da64e0229d5157f3583f5908994cf1d33e8837c8
stevenlim <stevenlimya@gmail.com>
2025-11-19 20:36:33 +0700

    y

--- movie-react/src/pages/movies/Movies.tsx
+++ movie-react/src/pages/movies/Movies.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "react"
 import { NavLink } from "react-router"
 import ApiClient from "../../utils/ApiClient"
+import { Button, Table } from "react-bootstrap"
 
 interface Movie{
     _id : string,
@@ -13,22 +14,63 @@ interface Movie{
 
 function Movies(){
     const [movies, setMovies] = useState<Movie[]>([])
-
+    const [loading, setLoading] = useState<boolean>(true)
+    
     const fetchMovies = useCallback(async () => {
+        setLoading(true)
         const response = await ApiClient.get("/movie")
 
         if(response.status == 200){
             setMovies(response.data.data)
+            setLoading(false)
         }
     }, [])
     useEffect(() => {
         fetchMovies()
     }, [fetchMovies])
 
+    const handleDelete =async (movieId: String) =>{
+        const response = await ApiClient.delete(`/movie/${movieId}`)
+
+        if (response.status == 200){
+            fetchMovies()
+        }
+    }
+
     return <div className="container mx-auto">
         <div className="d-flex justify-content-between mb-3">
         <h2>Movie page</h2>
         <NavLink to="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+     </div>
+     <div>
+        <Table striped bordered hover>
+            <thead>
+                <th>No</th>
+                <th>Judul</th>
+                <th>Tahun Rilis</th>
+                <th>Sutradara</th>
+                <th>Aksi</th>
+            </thead>
+            <tbody>
+                {
+                    loading && <tr>
+                        <td colSpan={5}>Loading.....</td>
+                    </tr>
+                }
+                {
+                    movies.length > 0 && movies.map((movie, index)=> {
+                        return <tr key={movie._id}>
+                            <td>{index + 1}</td>
+                            <td>{movie.judul}</td>
+                            <td>{movie.sutradara}</td>
+                            <td>{movie.tahunRilis}</td>
+                            <td><Button className="btn btn-danger" onClick={() => handleDelete(movie._id)}>Delete</Button></td>
+
+                        </tr>
+                    })
+                }
+            </tbody>
+        </Table>
      </div>
     </div>
 }
