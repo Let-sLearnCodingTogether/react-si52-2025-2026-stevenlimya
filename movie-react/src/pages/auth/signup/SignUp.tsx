4b11da1676100c3e1e1b44a811419557dc1d9f93
steven lim <valenjulyoarmandodavincylin_2226250045@mhs.mdp.ac.id>
2025-11-29 18:40:35 +0700

    Update SignUp.tsx

--- movie-react/src/pages/auth/signup/SignUp.tsx
+++ movie-react/src/pages/auth/signup/SignUp.tsx
@@ -1,11 +1,47 @@
+import { useState, type ChangeEvent, type FormEvent } from "react"
 import { Button, Form } from "react-bootstrap"
+import ApiClient from "../../../utils/ApiClient"
 
+interface SignUpForm{
+    username : string,
+    email : string,
+    password : string
+}
 function SignUp(){
-    return <div>
-        <Form>
+    const [form, setForm] = useState<SignUpForm>({
+        username : "",
+        email : "",
+        password : ""
+    })
+
+    const onHandleChange = (event : ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target
+
+        setForm({
+            ...form,
+            [name] : value
+        })
+    }
+
+    const onSubmit = async (event : FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        try {
+            const response = await ApiClient.post("/signup", form)
+
+            console.log(response);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return <div className="container mx-auto">
+        <h1>Sign Up</h1>
+        <Form onSubmit={onSubmit}>
             <Form.Group className ="mb-3" controlId="formUsername">
                 <Form.Label>Username</Form.Label>
                 <Form.Control
+                    value={form.username}
+                    onChange={onHandleChange}
                     name ="username" 
                     type="text"
                     placeholder="Username"/>
@@ -13,6 +49,8 @@ function SignUp(){
             <Form.Group  className ="mb-3" controlId="formEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control
+                    value={form.email}
+                    onChange={onHandleChange}
                     name ="email" 
                     type="text"
                     placeholder="Email"/>
@@ -20,13 +58,15 @@ function SignUp(){
             <Form.Group className ="mb-3" controlId="formPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
+                    value={form.password}
+                    onChange={onHandleChange}
                     name ="password" 
                     type="text"
                     placeholder="Password"/>
             </Form.Group>
 
             <Button type="submit" variant="primary">
-                Simpan
+                Sign Up
             </Button>
         </Form>
         </div>
